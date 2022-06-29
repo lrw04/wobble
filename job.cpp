30d1e323508074f1360191b415ced722b4d39d3f
Leran Wang <2428592483@qq.com>
2022-06-29 15:45:19 +0800

    Add logging calls

--- job.cpp
+++ job.cpp
@@ -1,17 +1,29 @@
 #include "job.h"
 
 #include <json.hpp>
+#include <loguru.hpp>
 #include <string>
 
 #include "util.h"
 
 Job Job::from_file(const std::filesystem::path& c) {
-    auto s = read_file(c);
-    auto r = nlohmann::json::parse(s);
+    std::string s;
+    nlohmann::json r;
+    try {
+        s = read_file(c);
+        r = nlohmann::json::parse(s);
+    } catch (const std::exception& e) {
+        ABORT_F("Failed to read job configuration: %s", e.what());
+    }
+
     Job res;
     res.cfg = c;
-    res.exe = std::filesystem::path(r.at("exe"));
-    res.name = r.at("name");
-    res.cycle = r.at("cycle");
+    try {
+        res.exe = std::filesystem::path(r.at("exe"));
+        res.name = r.at("name");
+        res.cycle = r.at("cycle");
+    } catch (const nlohmann::json::basic_json::out_of_range& e) {
+        ABORT_F("Invalid job configuration: %s", e.what());
+    }
     return res;
 }
--- report.cpp
+++ report.cpp
@@ -1,13 +1,14 @@
 #include "report.h"
 
-#define FMT_HEADER_ONLY 1
+#include <date/date.h>
+#include <date/tz.h>
 #include <fmt/format.h>
 
 #include <json.hpp>
+#include <loguru.hpp>
 #include <sstream>
 #include <stdexcept>
 
-#include "date.h"
 #include "util.h"
 
 using namespace date;
@@ -17,18 +18,18 @@ Report::Report(const std::filesystem::path& p) { f = p; }
 void Report::reg(const Job& job) {
     const std::lock_guard<std::mutex> lock(m);
     if (cont.count(job.name)) {
-        throw std::invalid_argument(
-            fmt::format("Job names collided between {} and {}.",
-                        cont[job.name].job.cfg.string(), job.cfg.string()));
+        ABORT_F("Job names collided between %s and %s",
+                cont[job.name].job.cfg.string().c_str(),
+                job.cfg.string().c_str());
     }
     cont[job.name] = {job, JobStatus::WAITING, clk.now(), -1};
 }
 
 void Report::update(const std::string& name, JobStatus st, int p, int ret) {
     const std::lock_guard<std::mutex> lock(m);
-    if (!cont.count(name))
-        throw std::invalid_argument(
-            fmt::format("Job not registered: {}.", name));
+    if (!cont.count(name)) {
+        ABORT_F("Job not registered: %s", name.c_str());
+    }
     cont[name].status = st;
     cont[name].update = clk.now();
     cont[name].pid = p;
@@ -41,7 +42,7 @@ void Report::update(const std::string& name, JobStatus st, int p, int ret) {
         r[k]["pid"] = cont[k].pid;
         r[k]["return_code"] = cont[k].ret;
         std::stringstream ss;
-        ss << cont[k].update;
+        ss << date::make_zoned(date::current_zone(), cont[k].update);
         r[k]["update"] = ss.str();
     }
     write_file(f, r.dump() + "\n");
